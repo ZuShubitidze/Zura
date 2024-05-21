bebb783571900e38dd7f45a4309e68be62a89d53
Zura Shubitidze <157276781+ZuShubitidze@users.noreply.github.com>
2024-05-21 21:03:21 +0400

    Add Homework

--- main.js
+++ main.js
@@ -1,32 +1,59 @@
-// 1
-function sum(a, b) {
-  if (typeof a === "number" && typeof b === "number") {
-    return a + b;
+let obj1 = {
+  name: "Temo",
+  age: 25,
+};
+let obj2 = {
+  name: "Lasha",
+  age: 21,
+};
+function compare(obj1, obj2) {
+  if (obj1.age > obj2.age) {
+    console.log(obj1.name);
+  } else if (obj1.age < obj2.age) {
+    console.log(obj2.name);
   } else {
-    return "ErrorInMatrix";
+    console.log("Equal");
   }
 }
+compare(obj1, obj2);
 
-console.log(sum(1, 2));
+//
 
-// 2
-function FatoCe(f) {
-  if (typeof f === "number") {
-    return "Celsius" + " " + (f - 32) / 1.8;
-  } else {
-    return "false";
-  }
+function randomNum() {
+  return parseInt(Math.random() * 10);
 }
 
-console.log(FatoCe(100));
-
-// 3
-function sum1(c, d) {
-  if (typeof c === "number" && typeof d === "number") {
-    return c * d - c;
-  } else {
-    return "false";
-  }
+let a = randomNum();
+while (a !== 3) {
+  a = randomNum();
+  console.log("a " + a);
 }
 
-console.log(sum1(5, 4));
+let b = randomNum();
+while (b !== 3) {
+  b = randomNum();
+  console.log("b " + b);
+}
+
+console.log(a.length);
+console.log(b.length);
+if (a.length > b.length) {
+  console.log("a wins");
+} else if (a.length === b.length) {
+  console.log("equal");
+} else {
+  console.log("b wins");
+}
+
+//
+
+let user = {
+  name: "Zura",
+  lastName: "Shubitidze",
+  age: 24,
+};
+
+const user1 = {
+  name: "Jil",
+  age: 29,
+};
