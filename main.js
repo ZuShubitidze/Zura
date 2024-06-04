822a52c642a3dba7287e1eaa3aa3785af2f274e3
Zura Shubitidze <157276781+ZuShubitidze@users.noreply.github.com>
2024-06-04 12:36:42 +0400

    Add Homework

--- main.js
+++ main.js
@@ -1,170 +1,239 @@
 //
-// Homework 30.05
+// Homework 06.01
 // 1
-const htmlDiv = document.createElement("div");
-const htmlbtn = document.createElement("button");
-const btnText = document.createTextNode("Button");
-const divText = document.createTextNode("Div Tag");
 
-htmlbtn.append(btnText);
-htmlDiv.append(divText);
-document.body.appendChild(htmlbtn);
-document.body.append(htmlDiv);
-htmlDiv.setAttribute("id", "divId");
-htmlbtn.setAttribute("id", "btnId");
+const button = document.createElement("button");
+button.innerText = "Button";
+button.classList.add("myBtn");
+document.body.append(button);
 
-const btn = document.querySelector("#btnId");
-btn.addEventListener("click", () => {
-  htmlDiv.style.display = "none";
+const modal = document.querySelector(".modal");
+
+const modalText = document.createElement("div");
+modalText.innerText = "This is my Modal";
+modalText.classList.add("modalText");
+modal.append(modalText);
+
+button.addEventListener("click", () => {
+  modal.style.display = "block";
 });
 
-console.log(document.body);
 // 1
 
 // 2
-const newHtmlDiv = document.createElement("div");
-newHtmlDiv.setAttribute("id", "card");
 
-const htmlH2 = document.createElement("h2");
-const htmlH2Text = document.createTextNode("Gandalf");
-htmlH2.append(htmlH2Text);
+const input = document.querySelector(".input");
+const inputBtn = document.querySelector(".inputBtn");
+const color = input.value;
 
-const htmlA = document.createElement("a");
-const htmlAText = document.createTextNode("Go to profile");
-htmlA.setAttribute("href", "#");
-htmlA.append(htmlAText);
+inputBtn.addEventListener("click", () => {
+  if (color === "red") {
+    document.body.style.backgroundColor = color;
+  } else if (color === "blue") {
+    document.body.style.backgroundColor = color;
+  } else if (color === "black") {
+    document.body.style.backgroundColor = color;
+  } else if (color === "white") {
+    document.body.style.backgroundColor = color;
+  } else if (color === "green") {
+    document.body.style.backgroundColor = color;
+  } else {
+    alert("wrong color");
+  }
+});
 
-document.body.append(newHtmlDiv);
-newHtmlDiv.appendChild(htmlH2);
-newHtmlDiv.appendChild(htmlA);
 // 2
 
 // 3
-const gameTitleH1 = document.createElement("h1"); // Header
-const gameTitle = document.createTextNode("Quiz Game");
-document.body.append(gameTitleH1);
-gameTitleH1.append(gameTitle);
-gameTitleH1.style.color = "blue";
-gameTitleH1.style.textAlign = "center"; // Header
 
-document.body.style.backgroundColor = "yellow"; // Background
+const inputNumber = document.querySelector(".inputNumber");
+const inputNumberBtn = document.querySelector(".inputNumberBtn");
+const arrInputNumber = Array.from(inputNumber);
+console.log(inputNumber.value.length, arrInputNumber.length);
 
-const question = document.createElement("h3"); // Question 1
-const questionText = document.createTextNode(
-  "The name of the most famous and decisive battle of Alexander the Great against Persian King Darius?"
-);
-document.body.append(question);
-question.append(questionText); // Question 1
+inputNumberBtn.addEventListener("click", (s) => {
+  s.preventDefault();
 
-const question2 = document.createElement("h3"); // Question 2
-const question2Text = document.createTextNode(
-  "Who united Germany in the second part of 19th century?"
-);
-document.body.append(question2);
-question2.append(question2Text);
-question2.style.display = "none"; // Question 2
-
-const answerUl = document.createElement("div"); // Answers 1
-const answer1 = document.createElement("button");
-const answer3 = document.createElement("button");
-const answer2 = document.createElement("button");
-const answer4 = document.createElement("button");
-const answer1Text = document.createTextNode("Issus");
-const answer2Text = document.createTextNode("Gaugamela");
-const answer3Text = document.createTextNode("Thermopylae");
-const answer4Text = document.createTextNode("Marathon");
-document.body.append(answerUl);
-answerUl.append(answer1, answer2, answer3, answer4);
-answer1.append(answer1Text);
-answer2.append(answer2Text);
-answer3.append(answer3Text);
-answer4.append(answer4Text); // Answers 1
-
-const answer21 = document.createElement("button"); // Answers 2
-const answer22 = document.createElement("button");
-const answer23 = document.createElement("button");
-const answer24 = document.createElement("button");
-const answer21Text = document.createTextNode("Adolf Hitler");
-const answer22Text = document.createTextNode("Otto Von Bismark");
-const answer23Text = document.createTextNode("Karl Heinz Rumenige");
-const answer24Text = document.createTextNode("Gerd Muller");
-answerUl.append(answer21, answer22, answer23, answer24);
-answer21.append(answer21Text);
-answer22.append(answer22Text);
-answer23.append(answer23Text);
-answer24.append(answer24Text);
-answer24.style.display = "none";
-answer23.style.display = "none";
-answer22.style.display = "none";
-answer21.style.display = "none"; // Answers 2
-
-answer1.addEventListener("click", () => {
-  // Colors and Dipslay
-  answer1.style.backgroundColor = "red";
-  answer1.style.transition = "5s";
-  answer1.style.display = "none";
-  answer2.style.display = "none";
-  answer4.style.display = "none";
-  answer3.style.display = "none";
-  question.style.display = "none";
-  question2.style.display = "initial";
-  answer21.style.display = "initial";
-  answer22.style.display = "initial";
-  answer23.style.display = "initial";
-  answer24.style.display = "initial";
-});
-answer2.addEventListener("click", () => {
-  answer2.style.backgroundColor = "green";
-  answer2.style.display = "none";
-  answer4.style.display = "none";
-  answer3.style.display = "none";
-  answer1.style.display = "none";
-  question.style.display = "none";
-  question2.style.display = "initial";
-  answer21.style.display = "initial";
-  answer22.style.display = "initial";
-  answer23.style.display = "initial";
-  answer24.style.display = "initial";
-});
-answer3.addEventListener("click", () => {
-  answer3.style.backgroundColor = "red";
-  answer4.style.display = "none";
-  answer2.style.display = "none";
-  answer3.style.display = "none";
-  answer1.style.display = "none";
-  question.style.display = "none";
-  question2.style.display = "initial";
-  answer21.style.display = "initial";
-  answer22.style.display = "initial";
-  answer23.style.display = "initial";
-  answer24.style.display = "initial";
-});
-answer4.addEventListener("click", () => {
-  answer4.style.backgroundColor = "red";
-  answer4.style.display = "none";
-  answer2.style.display = "none";
-  answer3.style.display = "none";
-  answer1.style.display = "none";
-  question.style.display = "none";
-  question2.style.display = "initial";
-  answer21.style.display = "initial";
-  answer22.style.display = "initial";
-  answer23.style.display = "initial";
-  answer24.style.display = "initial";
+  let sum = 0;
+  let average = 0;
+  for (let i = 0; i < inputNumber.value.length; i++) {
+    sum += inputNumber.value;
+    average = sum / inputNumber.value.length;
+  }
+  console.log(average);
 });
 
-answer21.addEventListener("click", () => {
-  answer21.style.backgroundColor = "red";
-});
-answer22.addEventListener("click", () => {
-  answer22.style.backgroundColor = "green";
-});
-answer23.addEventListener("click", () => {
-  answer23.style.backgroundColor = "red";
-});
-answer24.addEventListener("click", () => {
-  answer24.style.backgroundColor = "red";
-});
+// 3
+// Homework 06.01
+//
+
+//
+// Homework 30.05
+// 1
+// const htmlDiv = document.createElement("div");
+// const htmlbtn = document.createElement("button");
+// const btnText = document.createTextNode("Button");
+// const divText = document.createTextNode("Div Tag");
+
+// htmlbtn.append(btnText);
+// htmlDiv.append(divText);
+// document.body.appendChild(htmlbtn);
+// document.body.append(htmlDiv);
+// htmlDiv.setAttribute("id", "divId");
+// htmlbtn.setAttribute("id", "btnId");
+
+// const btn = document.querySelector("#btnId");
+// btn.addEventListener("click", () => {
+//   htmlDiv.style.display = "none";
+// });
+
+// console.log(document.body);
+// // 1
+
+// // 2
+// const newHtmlDiv = document.createElement("div");
+// newHtmlDiv.setAttribute("id", "card");
+
+// const htmlH2 = document.createElement("h2");
+// const htmlH2Text = document.createTextNode("Gandalf");
+// htmlH2.append(htmlH2Text);
+
+// const htmlA = document.createElement("a");
+// const htmlAText = document.createTextNode("Go to profile");
+// htmlA.setAttribute("href", "#");
+// htmlA.append(htmlAText);
+
+// document.body.append(newHtmlDiv);
+// newHtmlDiv.appendChild(htmlH2);
+// newHtmlDiv.appendChild(htmlA);
+// // 2
+
+// // 3
+// const gameTitleH1 = document.createElement("h1"); // Header
+// const gameTitle = document.createTextNode("Quiz Game");
+// document.body.append(gameTitleH1);
+// gameTitleH1.append(gameTitle);
+// gameTitleH1.style.color = "blue";
+// gameTitleH1.style.textAlign = "center"; // Header
+
+// document.body.style.backgroundColor = "yellow"; // Background
+
+// const question = document.createElement("h3"); // Question 1
+// const questionText = document.createTextNode(
+//   "The name of the most famous and decisive battle of Alexander the Great against Persian King Darius?"
+// );
+// document.body.append(question);
+// question.append(questionText); // Question 1
+
+// const question2 = document.createElement("h3"); // Question 2
+// const question2Text = document.createTextNode(
+//   "Who united Germany in the second part of 19th century?"
+// );
+// document.body.append(question2);
+// question2.append(question2Text);
+// question2.style.display = "none"; // Question 2
+
+// const answerUl = document.createElement("div"); // Answers 1
+// const answer1 = document.createElement("button");
+// const answer3 = document.createElement("button");
+// const answer2 = document.createElement("button");
+// const answer4 = document.createElement("button");
+// const answer1Text = document.createTextNode("Issus");
+// const answer2Text = document.createTextNode("Gaugamela");
+// const answer3Text = document.createTextNode("Thermopylae");
+// const answer4Text = document.createTextNode("Marathon");
+// document.body.append(answerUl);
+// answerUl.append(answer1, answer2, answer3, answer4);
+// answer1.append(answer1Text);
+// answer2.append(answer2Text);
+// answer3.append(answer3Text);
+// answer4.append(answer4Text); // Answers 1
+
+// const answer21 = document.createElement("button"); // Answers 2
+// const answer22 = document.createElement("button");
+// const answer23 = document.createElement("button");
+// const answer24 = document.createElement("button");
+// const answer21Text = document.createTextNode("Adolf Hitler");
+// const answer22Text = document.createTextNode("Otto Von Bismark");
+// const answer23Text = document.createTextNode("Karl Heinz Rumenige");
+// const answer24Text = document.createTextNode("Gerd Muller");
+// answerUl.append(answer21, answer22, answer23, answer24);
+// answer21.append(answer21Text);
+// answer22.append(answer22Text);
+// answer23.append(answer23Text);
+// answer24.append(answer24Text);
+// answer24.style.display = "none";
+// answer23.style.display = "none";
+// answer22.style.display = "none";
+// answer21.style.display = "none"; // Answers 2
+
+// answer1.addEventListener("click", () => {
+//   // Colors and Dipslay
+//   answer1.style.backgroundColor = "red";
+//   answer1.style.transition = "5s";
+//   answer1.style.display = "none";
+//   answer2.style.display = "none";
+//   answer4.style.display = "none";
+//   answer3.style.display = "none";
+//   question.style.display = "none";
+//   question2.style.display = "initial";
+//   answer21.style.display = "initial";
+//   answer22.style.display = "initial";
+//   answer23.style.display = "initial";
+//   answer24.style.display = "initial";
+// });
+// answer2.addEventListener("click", () => {
+//   answer2.style.backgroundColor = "green";
+//   answer2.style.display = "none";
+//   answer4.style.display = "none";
+//   answer3.style.display = "none";
+//   answer1.style.display = "none";
+//   question.style.display = "none";
+//   question2.style.display = "initial";
+//   answer21.style.display = "initial";
+//   answer22.style.display = "initial";
+//   answer23.style.display = "initial";
+//   answer24.style.display = "initial";
+// });
+// answer3.addEventListener("click", () => {
+//   answer3.style.backgroundColor = "red";
+//   answer4.style.display = "none";
+//   answer2.style.display = "none";
+//   answer3.style.display = "none";
+//   answer1.style.display = "none";
+//   question.style.display = "none";
+//   question2.style.display = "initial";
+//   answer21.style.display = "initial";
+//   answer22.style.display = "initial";
+//   answer23.style.display = "initial";
+//   answer24.style.display = "initial";
+// });
+// answer4.addEventListener("click", () => {
+//   answer4.style.backgroundColor = "red";
+//   answer4.style.display = "none";
+//   answer2.style.display = "none";
+//   answer3.style.display = "none";
+//   answer1.style.display = "none";
+//   question.style.display = "none";
+//   question2.style.display = "initial";
+//   answer21.style.display = "initial";
+//   answer22.style.display = "initial";
+//   answer23.style.display = "initial";
+//   answer24.style.display = "initial";
+// });
+
+// answer21.addEventListener("click", () => {
+//   answer21.style.backgroundColor = "red";
+// });
+// answer22.addEventListener("click", () => {
+//   answer22.style.backgroundColor = "green";
+// });
+// answer23.addEventListener("click", () => {
+//   answer23.style.backgroundColor = "red";
+// });
+// answer24.addEventListener("click", () => {
+//   answer24.style.backgroundColor = "red";
+// });
 // Colors and Dipslay
 
 // 3
